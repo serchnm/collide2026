44cce0841044aa0b921a14003f3bde996d4e0056
“Sergio” <sergio.navarro2501@gmail.com>
2026-08-11 22:52:15 -0700

    adding update for layout

---
+++ src/components/hero/hero.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import drSquatch from "../../image/conference.jpg";
+import "./hero.scss";
+
+
+const hero = ({ sharedData }) => {
+
+  return (
+    <section id="hero-section" className="hero">
+      <div className="hero-overlay" />
+
+      <div className="hero-content">
+        <span>{sharedData.staff}</span>
+        <div>
+          {/* <img src={drSquatch} alt={`project`} /> */}
+          <p>Bienvenidos a <br /> Collide</p>
+        </div>
+      </div>
+      
+    </section>
+  );
+};
+
+export default hero;
---
+++ src/mocks/shared-data.jsx
@@ -0,0 +1,267 @@
+export const hero_data = {
+  staff: "Hey Staff!",
+  welcome: "Bienvenido a\nCollide"
+}
+export const sharedData = {
+  name: "Sergio Bernardo Ortiz Navarro",
+  titles: [
+    "Shopify Developer",
+    "Front-end Developer",
+    "Full-stack Developer"
+  ],
+  social: [
+    {
+      "name": "github",
+      "url": "https://github.com",
+      "class": "fab fa-github"
+    },
+    {
+      "name": "instagram",
+      "url": "https://www.instagram.com",
+      "class": "fab fa-instagram"
+    }
+  ],
+  image: "serchProfile122.png"
+}
+
+export const about = {
+  title: 'ABOUT ME',
+  description: "👋 I'm Sergio Ortiz, a Web Developer with a passion for design and transforming mockups into engaging, functional digital experiences. I enjoy solving technical challenges and bringing creative ideas to life through clean and efficient code.",
+  subDescription: "Throughout my career, I've had the opportunity to work with diverse teams and clients, which has helped me continuously learn, adapt, and grow as a developer. I thrive in collaborative environments and enjoy taking on new challenges that allow me to expand my skills and deliver impactful solutions." ,
+  city: 'Location: Tijuana, Baja California, México'
+}
+
+export const skills = {
+  icons: [
+    {
+      "name": "HTML 5",
+      "class": "devicon-html5-plain",
+      "level": "95"
+    },
+    {
+      "name": "CSS 3",
+      "class": "devicon-css3-plain",
+      "level": "95"
+    },
+    {
+      "name": "JavaScript",
+      "class": "devicon-javascript-plain",
+      "level": "70"
+    },
+    {
+      "name": "Shopify",
+      "class": "fa-brands fa-shopify",
+      "level": "90"
+    },
+    
+    {
+      "name": "ReactJs",
+      "class": "devicon-react-plain",
+      "level": "80"
+    },
+    {
+      "name": "VueJs",
+      "class": "devicon-vuejs-plain",
+      "level": "80"
+    },
+    {
+      "name": "TypeScript",
+      "class": "devicon-typescript-plain",
+      "level": "90"
+    },
+    {
+      "name": "Sass",
+      "class": "devicon-sass-original",
+      "level": "75"
+    },
+    {
+      "name": "MaterialUI",
+      "class": "devicon-materialui-plain",
+      "level": "85"
+    },
+    {
+      "name": "NodeJs",
+      "class": "devicon-nodejs-plain",
+      "level": "60"
+    },
+    {
+      "name": "Express",
+      "class": "devicon-express-original-wordmark",
+      "level": "60"
+    },
+    {
+      "name": "MongoDb",
+      "class": "devicon-mongodb-plain",
+      "level": "60"
+    },
+    {
+      "name": "MySql",
+      "class": "devicon-mysql-plain",
+      "level": "60"
+    },
+    {
+      "name": "PostgreSql",
+      "class": "devicon-postgresql-plain",
+      "level": "60"
+    },
+    {
+      "name": "SqlServer",
+      "class": "devicon-azuresqldatabase-plain",
+      "level": "60"
+    },
+    {
+      "name": "Postman",
+      "class": "devicon-postman-plain",
+      "level": "60"
+    },
+    {
+      "name": "Git",
+      "class": "devicon-git-plain",
+      "level": "60"
+    },
+    {
+      "name": "Jira",
+      "class": "devicon-jira-plain",
+      "level": "60"
+    },
+    {
+      "name": "Figma",
+      "class": "devicon-figma-plain",
+      "level": "60"
+    },
+  ]
+}
+
+export const footer = {
+  social: [
+    {
+      "name": "github",
+      "url": "https://github.com/serchnm",
+      "class": "mdi:github"
+    },
+    {
+      "name": "instagram",
+      "url": "https://www.instagram.com/_serchnm",
+      "class": "mdi:instagram"
+    },
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/sergio-bernardo-ortiz-navarro-2bba8a164/",
+      "class": "mdi:linkedin"
+    }
+  ]
+}
+
+export const experience = {
+  info: [
+    {
+      company: "TryNow",
+      title: "Solutions Engineer",
+      years: "May 2025 - April 2026",
+      technologies: [
+          "Liquid",
+          "Html",
+          "REST API",
+          "ReactJS",
+          "JavaScript",
+          "Sass",
+          "Css",
+          "VueJs",
+          "NodeJs",
+          "Express"
+        ]
+    },
+    {
+      company: "DOVR Media",
+      title: "Shopify Developer Jr",
+      years: "Jan 2025 - May 2025",
+      technologies: [
+          "Liquid",
+          "Html",
+          "REST API",
+          "ReactJS",
+          "JavaScript",
+          "Sass",
+          "Css",
+          "VueJs",
+          "NodeJs",
+          "Express"
+        ]
+    },
+    {
+      company: "NTD Software",
+      title: "Shopify Developer Jr",
+      years: "July 2024 - Jan 2025",
+      technologies: [
+          "Liquid",
+          "Html",
+          "REST API",
+          "ReactJS",
+          "JavaScript",
+          "Sass",
+          "Css",
+          "VueJs",
+          "NodeJs",
+          "Express"
+        ]
+    },
+    {
+      company: "Arkus Nexus",
+      title: "Full-Stack Developer Mid",
+      years: "Feb 2021 - Jan 2024",
+      technologies: [
+          "Html",
+          "REST API",
+          "ReactJS",
+          "JavaScript",
+          "MaterialMUI",
+          "Bulma",
+          "MongoDb",
+          "SqlServer",
+          "Liquid",
+          "Sass",
+          "Css",
+          "Unity",
+          "VueJs",
+          "NextJs",
+          "Blender",
+          "NodeJs",
+          "Express"
+        ]
+    },
+    {
+      company: "Arkus Nexus",
+      title: "Full-Stack Developer Jr",
+      years: "Nov 2019 - Jan 2021",
+      technologies: [
+          "Html",
+          "REST API",
+          "ReactJS",
+          "JavaScript",
+          "MaterialMUI",
+          "Css",
+          "MongoDb",
+          "SqlServer",
+          "NodeJs",
+          "Express",
+          "C#"
+        ]
+    },
+    {
+      company: "Arkus Nexus",
+      title: "Full-Stack Developer Intern",
+      years: "July 2019 - Oct 2019",
+      technologies: [
+          "Html",
+          "REST API",
+          "JavaScript",
+          "VueJs",
+          "Bootstrap",
+          "Css",
+          "MongoDb",
+          "NodeJs",
+          "Express"
+        ]
+    },
+  ]
+}
